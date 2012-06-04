e6a8e93cff4810feb87f937e72ffd3d02bd94ab9
Juho Vepsalainen <bebraw@gmail.com>
2012-06-04 15:45:31 +0300

    Add rest-sugar-client dep

--- frontend/server.js
+++ frontend/server.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 var express = require('express');
-var rest = require('request');
-var fkit = require('funkit');
+var client = require('rest-sugar-client');
 
 // TODO: move to config and hide from repo
 var APIKEY = 'dummy';
@@ -13,17 +12,18 @@ function main() {
     // TODO: set up ssl for production
     var app = express.createServer();
 
-    getMeta(BACKEND_URL + '?apikey=' + APIKEY, function(d) {
-        var api = constructAPI(BACKEND_URL, d);
+    client.api(BACKEND_URL + '?apikey=' + APIKEY, function(api) {
+        console.log('success');
+        console.log(api);
 
         /* demo
         api.task.create({apikey: APIKEY, description: 'dummy todo'},
             function(d) {console.log('ok', d);},
-            function(e) {console.log('err', e);}
+            function(e) {console.log('err, e);}
         );
         */
 
-        // initServer(api);
+        // initServer(api)
     }, function(err) {console.log(err);});
 }
 
@@ -37,60 +37,5 @@ function initServer(api) {
     var port = 4000;
     console.log('Surf to localhost:' + port);
     app.listen(port);
-}
-
-function getMeta(url, okCb, errCb) {
-    rest.get(url, function(err, d) {
-        if(err) errCb(err);
-        else okCb(JSON.parse(d.body));
-    });
-}
-
-function constructAPI(url, d) {
-    // TODO: make sure url has a trailing slash
-    var ret = {};
-
-    for(var k in d) {
-        var v = d[k];
-        var name = k.slice(0, -1);
-        var r = url + k;
-
-        // TODO: collection ops (ie. boards(id).columns.<op>)
-        ret[name] = op('get', r);
-        ret[name].count = op('get', r + 'count');
-        ret[name].create = op('post', r);
-        ret[name].update = op('put', r);
-        ret[name]['delete'] = op('del', r);
-    }
-
-    function op(method, r) {
-        return function(o, okCb, errCb) {
-            if(fkit.isString(o)) {
-                rest.get({url: r + o, qs: {method: method}}, handle(okCb, errCb));
-            }
-            else {
-                o.method = method;
-                rest.get(r + '?' + toQ(o), handle(okCb, errCb));
-            }
-        };
-    }
-
-    function handle(okCb, errCb) {
-        return function(err, d) {
-            if(err || d.statusCode != 200) errCb(err || d.body);
-            else okCb(JSON.parse(d.body));
-        };
-    }
-
-    function toQ(o) {
-        var fields = o.fields? o.fields.join(','): '';
-        delete o.fields;
-
-        return fields + fkit.otozip(o).map(function(v) {
-            return v[0] + '=' + v[1].split(' ').join('+');
-        }).join('&');
-    }
-
-    return ret;
 }
 
